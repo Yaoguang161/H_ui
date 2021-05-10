3430e595fc11ff63ee193c58f0d1779587389b32
yaoguang161 <yaoguang161@outlook.com>
2021-05-10 21:48:17 +0800

    add

--- day01/10.cpp
+++ day01/10.cpp
@@ -1,4 +1,31 @@
 //
 // Created by Akie on 2021/4/17.
-//
-
+//3417. 砝码称重
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+const int N = 110, M = 2e5+10;
+int n;
+int sum;
+int w[N];
+bool f[N][M];
+int main(){
+    scanf("%d",&n);
+    for(int i = 1; i <= n; i ++){
+        scanf("%d",&w[i]);
+        sum += w[i];
+    }
+    f[0][0] = true;
+    for(int i = 1; i <= n; i++){
+        for(int j = 0; j <= sum; j ++){
+            f[i][j] = f[i-1][j]||f[i-1][j+w[i]] ||f[i-1][abs(j-w[i])];
+        }
+    }
+    int ans= 0;
+    for(int i = 1 ; i <=sum; i++){
+        if(f[n][i]) ans ++;
+    }
+    cout <<ans;
+    return 0;
+}
--- day01/9.cpp
+++ day01/9.cpp
@@ -1,11 +1,32 @@
 //
 // Created by Akie on 2021/4/17.
-//
+//3417. 砝码称重
 #include<iostream>
 using namespace  std;
+const int  N = 110,M =200020;
+int B = M / 2;
+int n;
+int m;
+int w[N];
+bool f[N][M];
 int main(){
-
-
-
+    scanf("%d",&n);
+    for(int i = 1 ;i <= n; i++){
+        scanf("%d",&w[i]);
+        m += w[i];
+    }
+    f[0][B] = true;
+    for(int i = 1; i <= n; i++){
+        for(int j = -m; j <= m; j ++){
+            f[i][j+B] = f[i-1][j+B];
+            if(j-w[i] >= -m) f[i][j+B] |= f[i-1][j-w[i]+B];
+            if(j+w[i] <= m)  f[i][j+B] |= f[i-1][j+w[i]+B];
+        }
+    }
+    int res = 0;
+    for(int j = 1; j <= m; j ++){
+        if(f[n][j+B]) res ++;
+    }
+    printf("%d",res);
     return 0;
 }
